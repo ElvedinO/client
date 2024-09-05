771dde3ff1227d0b2f791671ddfeaf026df2a01b
ElvedinO <vedo_o.95@hotmail.com>
2024-09-05 23:37:04 +0200

    strapi/mysql integration

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutlineOutlined';
@@ -7,8 +7,11 @@ import ShoppingCartOutliedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
 
 import './Navbar.scss';
+import Cart from '../Cart/Cart';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className='navbar'>
@@ -71,13 +74,15 @@ const Navbar = () => {
               <SearchIcon />
               <PersonOutlineIcon />
               <FavoriteBorderIcon />
-              <div className='cartIcon'>
+              <div className='cartIcon' onClick={() => setOpen(!open)}>
                 <ShoppingCartOutliedIcon />
                 <span>0</span>
               </div>
             </div>
           </div>
         </div>
+
+        {open && <Cart />}
       </div>
     </>
   );
---
+++ client/src/makeRequest.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiToken = process.env.REACT_APP_API_TOKEN;
+
+export const makeRequest = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${apiToken}`,
+  },
+});
